affdd2e1022e7b0dd3f447e3a5fc849f2d5fe3a1
RafitJ <rafit.jamil@gmail.com>
2019-01-13 12:34:08 -0500

    Implemented CoreML

--- Fashionably/Controller/MenuViewController.swift
+++ Fashionably/Controller/MenuViewController.swift
@@ -14,71 +14,94 @@ import RealmSwift
 class MenuViewController: UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate {
     
     
-//     let imagePicker = UIImagePickerController()
+    let imagePicker = UIImagePickerController()
     
     let realm = try! Realm()
+    
+    let types = ["T-shirt", "trouser", "dress", "coat", "sandal", "shirt", "sneaker", "boot", "shoes", "pants", "sweater", "jacket", "shorts"]
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//                imagePicker.delegate = self
-//                imagePicker.allowsEditing = false
-//                imagePicker.sourceType = .camera
+                imagePicker.delegate = self
+                imagePicker.allowsEditing = false
+                imagePicker.sourceType = .photoLibrary
     }
     
     //Image picker methods
-//        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-//
-//
-//            if let userPickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
-//                guard let ciImage = CIImage(image: userPickedImage)else {
-//                    fatalError("could not convert")
-//                }
-//
-//                detect(image: ciImage)
-//                ImageView.image = userPickedImage
-//            }
-//
-//            imagePicker.dismiss(animated: true, completion: nil)
-//        }
-//
-//        func detect(image: CIImage){
-//
-//            guard let model = try? VNCoreMLModel(for: FlowerClassifier().model) else {
-//                fatalError("cannot import")
-//            }
-//
-//            let request = VNCoreMLRequest(model: model) { (request, error) in
-//                guard let classification = request.results?.first as? VNClassificationObservation else {
-//                    fatalError("No flower found")
-//                }
-//
-//                self.navigationItem.title = classification.identifier.capitalized
-//                self.requestInfo(flowerType: classification.identifier)
-//
-//            }
-//
-//            let handler = VNImageRequestHandler(ciImage: image)
-//
-//            do {
-//                try handler.perform([request])
-//            } catch {
-//                print(error)
-//            }
-//        }
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+
+
+            if let userPickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+                guard let ciImage = CIImage(image: userPickedImage)else {
+                    fatalError("could not convert")
+                }
+
+                detect(image: ciImage)
+            }
+
+            imagePicker.dismiss(animated: true, completion: nil)
+        }
+
+        func detect(image: CIImage){
+
+            guard let model = try? VNCoreMLModel(for: VGG16().model) else {
+                fatalError("cannot import")
+            }
+
+            let request = VNCoreMLRequest(model: model) { (request, error) in
+                guard let results = request.results as? [VNClassificationObservation] else {
+                    fatalError("error processing image")
+                }
+                
+                 let mainResults = results[0...4]
+                for results in mainResults {
+                    if self.types.contains(results.identifier){
+                        let newClothing = Clothing()
+                        newClothing.type = results.identifier
+                        newClothing.warmthRating = self.setWarmthRating(type: newClothing.type)
+                        newClothing.formalityRating = self.setFormalRating(type: newClothing.type)
+                        do {
+                            try self.realm.write {
+                                self.realm.add(newClothing)
+                            }
+                        }catch {
+                            print("Error saving clothing \(error)")
+                        }
+                    }
+                }
+//                    if mainResults.contains("hotdog") {
+//                        self.navigationItem.title = "Hotdog!"
+//                    } else {
+//                        self.navigationItem.title = "Not Hotdog!"
+//                    }
+                
+            }
+
+            let handler = VNImageRequestHandler(ciImage: image)
+
+            do {
+                try handler.perform([request])
+            } catch {
+                print(error)
+            }
+        }
     
     func setWarmthRating(type: String) -> Double {
         let warmthValues : [String:Double] = [
-            "T-Shirt" : 0.15,
-            "Trouser": 0.25,
-            "Pullover": 0.35,
-            "Dress": 0.15,
-            "Coat": 0.50,
-            "Sandal": 0.10,
-            "Shirt": 0.20,
-            "Sneaker": 0.50,
-            "Bag": 0.00,
-            "Ankle Boot": 1.00
+            "T-shirt" : 0.15,
+            "trouser": 0.5,
+            "dress": 0.15,
+            "coat": 0.50,
+            "sandal": 0.10,
+            "shirt": 0.20,
+            "sneaker": 0.50,
+            "boot": 1.00,
+            "shoe": 0.4,
+            "pants": 0.45,
+            "jeans": 0.5,
+            "sweater": 0.45,
+            "shorts": 0.3
         ]
         
         return warmthValues[type]!
@@ -86,16 +109,19 @@ class MenuViewController: UIViewController, UIImagePickerControllerDelegate,UINa
     
     func setFormalRating(type: String) -> Double {
         let formalValues : [String:Double] = [
-            "T-Shirt" : 0.2,
-            "Trouser": 0.8,
-            "Pullover": 0.2,
-            "Dress": 0.8,
-            "Coat": 0.5,
-            "Sandal": 0.2,
-            "Shirt": 0.6,
-            "Sneaker": 0.8,
-            "Bag": 0.9,
-            "Ankle Boot": 0.5
+            "T-shirt" : 0.2,
+            "trouser": 0.8,
+            "dress": 0.8,
+            "coat": 0.5,
+            "sandal": 0.2,
+            "shirt": 0.6,
+            "sneaker": 0.4,
+            "boot": 1.00,
+            "shoe": 0.4,
+            "pants": 0.45,
+            "jeans": 0.4,
+            "sweater": 0.2,
+            "shorts": 0.2
         ]
         
         return formalValues[type]!
@@ -111,32 +137,21 @@ class MenuViewController: UIViewController, UIImagePickerControllerDelegate,UINa
             "Sandal": "footwear",
             "Shirt": "upper",
             "Sneaker": "footwear",
-            "Bag": "other",
-            "Ankle Boot": "footwear"
+            "boot": "footwear",
+            "shoe": "footwear",
+            "pants": "lower",
+            "jeans": "lower",
+            "sweater": "upper",
+            "shorts": "lower"
         ]
         return categories[type]!
     }
     
-    
-    @IBAction func addClothingPressed(_ sender: UIButton) {
-        let newClothing = Clothing()
-        newClothing.type = getClothingType()
-        newClothing.warmthRating = setWarmthRating(type: newClothing.type)
-        newClothing.formalityRating = setFormalRating(type: newClothing.type)
-        do {
-            try realm.write {
-                realm.add(newClothing)
-            }
-        }catch {
-                print("Error saving clothing \(error)")
-            }
-        
+
+    @IBAction func addClothingButtonPressed(_ sender: UIButton) {
+        present(imagePicker, animated: true, completion: nil)
     }
     
-    
-    func getClothingType() -> String {
-        return "Heh"
-    }
     
 
 }
