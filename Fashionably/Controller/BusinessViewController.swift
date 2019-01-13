744e72e7196f5da8ed64ba76faa9c5586a3f7774
Rikin Patel <rikinpatel10@yahoo.com>
2019-01-13 14:51:13 -0500

    asdf

---
+++ Fashionably/Controller/BusinessViewController.swift
@@ -0,0 +1,57 @@
+//
+//  BusinessViewController.swift
+//  Fashionably
+//
+//  Created by Rikin Patel on 2019-01-13.
+//  Copyright © 2019 Hrithvik  Alex. All rights reserved.
+//
+
+import UIKit
+
+class BusinessViewController: UIViewController {
+    
+    
+    @IBOutlet weak var image1: UIImageView!
+    @IBOutlet weak var image2: UIImageView!
+    @IBOutlet weak var image3: UIImageView!
+    @IBOutlet weak var image4: UIImageView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func Lbutton1(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func Rbutton1(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func LButton2(_ sender: Any) {
+    }
+    
+    
+    @IBAction func RButton2(_ sender: Any) {
+    }
+    
+    
+    @IBAction func LButton3(_ sender: Any) {
+    }
+    
+    @IBAction func RButton3(_ sender: Any) {
+    }
+    
+    
+    @IBAction func LButton4(_ sender: Any) {
+    }
+    
+    
+    @IBAction func RButton4(_ sender: Any) {
+    }
+    
+}
