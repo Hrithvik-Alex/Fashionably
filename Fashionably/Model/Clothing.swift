20fc741f52803ae6779de6aa332bd5846af613df
Rikin Patel <rikinpatel10@yahoo.com>
2019-01-13 12:37:34 -0500

    Working on the Algorithms

--- Fashionably/Model/Clothing.swift
+++ Fashionably/Model/Clothing.swift
@@ -10,9 +10,24 @@ import Foundation
 import RealmSwift
 
 class Clothing: Object {
+    @objc dynamic var image: UIImage
     @objc dynamic var type : String = ""// Upper body, lower body, feet
     @objc dynamic var warmthRating : Double = 0.0
     @objc dynamic var category : String = "" // trousers, pullovers, coats , etc.
     @objc dynamic var formalityRating : Double = 0.0
 
 }
+
+let UpperBodyClosetImages: [UIImage] = []
+let UpperBodyClosetClasses: [Object] = []
+
+
+let UpperBodyDisplayImages: [UIImage] = []
+
+
+for  i in range(UpperBodyClosetClasses) {
+    if UpperBodyClosetClasses.warmth <= 0.5 {
+        UpperBodyDisplayImages.append(UpperBodyClosetClasses[i].image)
+    }
+}
+
