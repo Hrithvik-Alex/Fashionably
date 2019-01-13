d4fa14a0438cffb8e05ac59b407f169103d8a781
RafitJ <rafit.jamil@gmail.com>
2019-01-13 14:35:28 -0500

    Added more View Controllers

--- Fashionably/Controller/OutfitViewController.swift
+++ Fashionably/Controller/OutfitViewController.swift
@@ -15,108 +15,34 @@ class OutfitViewController: UIViewController {
     
     var clothes : Results<Clothing>!
     
+    @IBOutlet weak var firstImage: UIImageView!
     
-    @IBOutlet weak var image3: UIImageView!
+
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         loadClothes()
-        photoCircle(image: image3!)
+        firstImage.image = "sweater-2.jpg"
+        secondImage.image = "sweater-2.jpg"
+        thirdImage.image = "sweater-2.jpg"
+        fourthImage.image = "sweater-2.jpg"
 
     }
     
-    func photoCircle(image: UIImageView){
-        image.layer.borderWidth = 1
-        image.layer.masksToBounds = false
-        image.layer.borderColor = UIColor.black.cgColor
-        image.layer.cornerRadius = image.frame.height/2
-        image.clipsToBounds = true
-    }
+    
     
     
     func loadClothes() {
         clothes = realm.objects(Clothing.self)
     }
     
-    
     @IBAction func firstRightPressed(_ sender: UIButton) {
         
     }
     
     
-    @IBAction func firstLeftPressed(_ sender: UIButton) {
-        
-    }
-    
-    
-    @IBAction func secondRightPressed(_ sender: UIButton) {
-        
-    }
-    
-    
-    @IBAction func secondLeftPressed(_ sender: UIButton) {
-        
-    }
-    
-    
-    @IBAction func thirdRightPressed(_ sender: UIButton) {
-        
-    }
-    
-    
-    @IBAction func thirdLeftPressed(_ sender: UIButton) {
-        
-    }
-    
-    
-    @IBAction func fourthRightPressed(_ sender: UIButton) {
-        
-    }
-    
-    
-    @IBAction func fourthLeftPressed(_ sender: UIButton) {
-        
-    }
 }
 
-
-
-func filter1(ranger: Array<Clothing>, storer: Array<UIImage>, x:  int){
-    for  i in range(ranger) {
-        if ranger.formality <= 0.5 {
-            storer.append(ranger[i].image)
-        }
-    }
-    
-}
-
-//Creating a list  of UpperBodyImages to display
-//let UpperBodyClosetClasses: [Object] = []
-
-let UpperBodyImagesV1: [UIImage] = []
-
-filter1(UpperBodyClosetClasses, UpperBodyImagesV1, 0.5)
-
-//Creating a list of LowerBodyImages to display
-//let LowerBodyClosetClasses: [Object] = []
-
-
-let LowerBodyImagesV1: [UIImage] = []
-
-filter1(LowerBodyClosetClasses, LowerBodyImagesV1, 0.5)
-
-//Creating a list  of OuterWearImages to display
-//let OuterWearClosetClasses: [Object] = []
-
-let OuterWearImagesV1: [UIImage] = []
-filter1(OuterWearClosetClasses, OuterWearImagesV1, 0.5)
-
-
-//Creating a list  of ShoesImages to display
-//let ShoesClosetClasses: [Object] = []
-
-let ShoesImagesV1: [UIImage] = []
-filter1(ShoesClosetClasses, ShoesImagesV1, 0.5)
 
 
